f2bab255e849ba0302106c6d6a5a64cd964c12e8
dshawth <dshawth@pm.me>
2020-05-19 15:10:52 -0400

    separated results for crypto experiment

--- experiments/full_cluster/crypto/reduce.sh
+++ experiments/full_cluster/crypto/reduce.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 for i in {0..9}; do
-    grep aes-cbc r$i.txt > c$i.txt
+    grep aes-cbc r$i.txt > aes/c$i.txt
+    grep serpent-cbc r$i.txt > serpent/c$i.txt
+    grep twofish-cbc r$i.txt > twofish/c$i.txt
 done
