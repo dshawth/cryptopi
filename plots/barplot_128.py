c0dfb9b085ce109e0182e26fb30713a5343d3120
Michael Kapralos <michael.kapralos@westpoint.edu>
2020-05-26 11:24:46 -0400

    MRD added

--- plots/barplot_128.py
+++ plots/barplot_128.py
@@ -35,19 +35,19 @@ pi_aecrypt = [95.26, 197.74, 299.99, 401.5, 503.73, 602.6, 703.38, 806.23, 909.0
 ######################## DECRYPT DATA #########################
 
 ###SERPENT
-#nuc_secrypt =  610.5 
-#hpc_secrypt = 360.6
-#pi_secrypt = [56.36, 116.87, 177.2, 236.77, 297.08, 357.06, 416.78, 477.14, 537.23,594.79]
+nuc_secrypt =  610.5 
+hpc_secrypt = 360.6
+pi_secrypt = [56.36, 116.87, 177.2, 236.77, 297.08, 357.06, 416.78, 477.14, 537.23,594.79]
 
 ###TWOFISH data
-#nuc_tecrypt = 341.1
-#hpc_tecrypt = 386.6 
-#pi_tecrypt = [83.62, 173.32, 262.44, 350.58, 439.68, 528.52, 616.56, 705.74, 794.89, 880.6]
+nuc_tecrypt = 341.1
+hpc_tecrypt = 386.6 
+pi_tecrypt = [83.62, 173.32, 262.44, 350.58, 439.68, 528.52, 616.56, 705.74, 794.89, 880.6]
 
 ###AES DATA
-#nuc_aecrypt =  2889.1
-#hpc_aecrypt = 3663.1
-#pi_aecrypt = [112.01, 232.32, 352.67, 471.62, 591.98, 710.41,  827.78, 949.13, 1069.88, 1183.05]
+nuc_aecrypt =  2889.1
+hpc_aecrypt = 3663.1
+pi_aecrypt = [112.01, 232.32, 352.67, 471.62, 591.98, 710.41,  827.78, 949.13, 1069.88, 1183.05]
 
 
 #creates seperate plots for encrypt and decrypt --uncomment the one you need
@@ -60,15 +60,15 @@ ax.set_xlabel('Number of Pis')
 bar_width=0.2
 
 ax.bar(x, pi_secrypt, bar_width, color='c', label="Pi Serpent")
-plt.axhline(y=hpc_secrypt, color='c', linestyle='-', label="HEDT Serpent", linewidth=2)
+plt.axhline(y=hpc_secrypt, color='c', linestyle='-', label="MRD Serpent", linewidth=2)
 plt.axhline(y=nuc_secrypt, color='c', linestyle='--', label="NUC Serpent", linewidth=2)
 
 ax.bar(x+bar_width, pi_aecrypt, bar_width, color='k', label="Pi AES")
-plt.axhline(y=hpc_aecrypt, color='k', linestyle='-', label="HEDT AES", linewidth=2)
+plt.axhline(y=hpc_aecrypt, color='k', linestyle='-', label="MRD AES", linewidth=2)
 plt.axhline(y=nuc_aecrypt, color='k', linestyle='--', label="NUC AES", linewidth=2)
 
 ax.bar(x+bar_width+bar_width, pi_tecrypt, bar_width, color='g', label="Pi Twofish")
-plt.axhline(y=hpc_tecrypt, color='g', linestyle='-', label="HEDT Twofish", linewidth=2)
+plt.axhline(y=hpc_tecrypt, color='g', linestyle='-', label="MRD Twofish", linewidth=2)
 plt.axhline(y=nuc_tecrypt, color='g', linestyle='--', label="NUC Twofish", linewidth=2)
 
 
@@ -83,7 +83,7 @@ ax.legend([handles[idx] for idx in order],[labels[idx] for idx in order],
 bbox_to_anchor=(0., 1.02, 1., 0.2), loc='lower left', ncol=3,  mode="expand", borderaxespad=0.)
 plt.subplots_adjust(top=0.8)
 
-ax.set_ylim([0,2000])
+ax.set_ylim([0,4000])
 
 
 
--- plots/barplot_256.py
+++ plots/barplot_256.py
@@ -28,21 +28,21 @@ pi_aecrypt = [71.51, 148.29, 224.71, 300.1, 376.4, 452.16, 527.57, 603.79, 679.9
 ######################## DECRYPT DATA #########################
 
 ###SERPENT DATA
-nuc_secrypt = 581.1
-hpc_secrypt = 374.3 
-pi_tecrypt = [56.94, 117.47, 177.78, 237.44, 297.69, 357.55, 417.43, 477.64, 538.07, 596.07]
+#nuc_secrypt = 581.1
+#hpc_secrypt = 374.3 
+#pi_tecrypt = [56.94, 117.47, 177.78, 237.44, 297.69, 357.55, 417.43, 477.64, 538.07, 596.07]
 
 
 ###TWOFISH data
-nuc_tecrypt = 340.4
-hpc_tecrypt = 378.7
-pi_tdcrypt = [84.24, 174.39, 263.29, 351.52, 440.33, 528.99, 616.92, 706.24, 795.29, 881.62]
+#nuc_tecrypt = 340.4
+#hpc_tecrypt = 378.7
+#pi_tdcrypt = [84.24, 174.39, 263.29, 351.52, 440.33, 528.99, 616.92, 706.24, 795.29, 881.62]
 
 
 ###AES DATA
-nuc_aecrypt = 2335.6 
-hpc_aecrypt =  3164.7
-pi_aecrypt = [85.07, 176.64, 267.42, 357.07, 447.79, 538.5, 628.38, 719.12, 810.24, 897.66]
+#nuc_aecrypt = 2335.6 
+#hpc_aecrypt =  3164.7
+#pi_aecrypt = [85.07, 176.64, 267.42, 357.07, 447.79, 538.5, 628.38, 719.12, 810.24, 897.66]
 
 
 #creates seperate plots for encrypt and decrypt --uncomment the one you need
@@ -59,15 +59,15 @@ bar_width=0.2
 ax.bar(x, pi_aecrypt, bar_width, color='c', label="Pi Serpent")
 
 
-plt.axhline(y=hpc_secrypt, color='c', linestyle='-', label="HEDT Serpent", linewidth=2)
+plt.axhline(y=hpc_secrypt, color='c', linestyle='-', label="MRD Serpent", linewidth=2)
 plt.axhline(y=nuc_secrypt, color='c', linestyle='--', label="NUC Serpent", linewidth=2)
 
 ax.bar(x+bar_width, pi_aecrypt, bar_width, color='k', label="Pi AES")
-plt.axhline(y=hpc_aecrypt, color='k', linestyle='-', label="HEDT AES", linewidth=2)
+plt.axhline(y=hpc_aecrypt, color='k', linestyle='-', label="MRD AES", linewidth=2)
 plt.axhline(y=nuc_aecrypt, color='k', linestyle='--', label="NUC AES", linewidth=2)
 
 ax.bar(x+bar_width+bar_width, pi_tecrypt, bar_width, color='g', label="Pi Twofish")
-plt.axhline(y=hpc_tecrypt, color='g', linestyle='-', label="HEDT Twofish", linewidth=2)
+plt.axhline(y=hpc_tecrypt, color='g', linestyle='-', label="MRD Twofish", linewidth=2)
 plt.axhline(y=nuc_tecrypt, color='g', linestyle='--', label="NUC Twofish", linewidth=2)
 
 
@@ -83,7 +83,7 @@ ax.legend([handles[idx] for idx in order],[labels[idx] for idx in order],
 bbox_to_anchor=(0., 1.02, 1., 0.2), loc='lower left', ncol=3,  mode="expand", borderaxespad=0.)
 plt.subplots_adjust(top=0.8)
 
-ax.set_ylim([0,4000])
+ax.set_ylim([0,1500])
 #plt.xlim([0,x.size])
 #plt.axis('tight')
 
