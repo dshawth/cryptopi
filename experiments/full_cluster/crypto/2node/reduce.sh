586a27a4481e84a4f707c4b41f4e486ef648e453
dshawth <dshawth@pm.me>
2020-05-27 08:00:09 -0400

    2 node for code audit

--- experiments/full_cluster/crypto/2node/reduce.sh
+++ experiments/full_cluster/crypto/2node/reduce.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# make directories to hold the raw data
 mkdir -p aes serpent twofish
 
+# for each iteration of cryptsetup benchmark
 for i in {0..9}; do
     grep aes-cbc raw/r$i.txt > aes/c$i.txt
     grep serpent-cbc raw/r$i.txt > serpent/c$i.txt
--- experiments/full_cluster/crypto/2node/sum.py
+++ experiments/full_cluster/crypto/2node/sum.py
@@ -2,21 +2,30 @@
 
 import os
 
+# number of iterations for `cryptsetup benchmark`
 runs = 10
 
 if os.path.exists('results.txt'):
     os.remove('results.txt')
 
+# for each algorithm
 for alg in ['aes', 'serpent', 'twofish']:
     enc128 = []
     dec128 = []
     enc256 = []
-    dec256 = []  
+    dec256 = []
 
+    # for each file in the directory for the current algorithm
     for fileName in sorted(os.listdir(alg)):
+        # open the file
         with open(os.path.join(alg, fileName)) as inFile:
+            # for each line in the file
             for line in inFile.readlines():
                 line = line.split()
+                print(line) # debug, example line below
+                #  cipher     key     encrypt          decrypt
+                # ['aes-cbc', '128b', '44.9', 'MiB/s', '54.4', 'MiB/s']
+                # if [1] see above
                 if line[1] == '128b':
                     enc128.append(float(line[2]))
                     dec128.append(float(line[4]))
